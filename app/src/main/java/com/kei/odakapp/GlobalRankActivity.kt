cb6ed0c7458a534bf1a29cafebe03fe024a85db2
priyanka uzlah <priyankauzlah@gmail.com>
2021-01-19 21:41:27 +0700

    global rank layout

---
+++ app/src/main/java/com/kei/odakapp/GlobalRankActivity.kt
@@ -0,0 +1,11 @@
+package com.kei.odakapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GlobalRankActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_global_rank)
+    }
+}
