58f79afc634a6080612c51a34ce05c4bc9f798ad
ayeshaazeema <ayesha.azeema@gmail.com>
2021-01-23 14:07:13 +0700

    Planner and Add Task

---
+++ app/src/main/java/com/kei/odakapp/AddTaskActivity.kt
@@ -0,0 +1,13 @@
+package com.kei.odakapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+
+        supportActionBar?.hide()
+    }
+}
