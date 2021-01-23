e045707956c2e9219e85a0289386a27bb5207017
keishaputritheanny <pkeisha47@gmail.com>
2021-01-23 14:07:13 +0700

    Logic signin, register, stuff

--- app/src/main/java/com/kei/odakapp/HomeFragment.kt
+++ app/src/main/java/com/kei/odakapp/HomeFragment.kt
@@ -1,20 +1,20 @@
 package com.kei.odakapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_first_onboarding.*
 
 class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
         }
     }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
