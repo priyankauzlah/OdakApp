afa57b4efbe318af06a03b93e356da1c2071453f
keishaputritheanny <pkeisha47@gmail.com>
2021-01-23 22:13:40 +0700

    test

--- app/src/main/java/com/kei/odakapp/home/HomeFragment.kt
+++ app/src/main/java/com/kei/odakapp/home/HomeFragment.kt
@@ -17,6 +17,7 @@ class HomeFragment : Fragment() {
         arguments?.let {
 
         }
+        
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
